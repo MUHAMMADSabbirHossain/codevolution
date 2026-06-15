43f21e000328231b4a029cbf9977f93bdcf68ab5
Muhammad Sabbir Hossain Bin Ahmad <dustobalok000@gmail.com>
2026-06-15 15:40:24 +0600

    complete: Next.js 15 Tutorial - 19 - Link Component.

--- nextjs-15-beg-adv/routing-demo/src/app/products/page.tsx
+++ nextjs-15-beg-adv/routing-demo/src/app/products/page.tsx
@@ -1,13 +1,29 @@
-export default function Products() {
+import Link from "next/link";
+
+export default function Products(): React.JSX.Element {
+  const productId = 100;
+
   return (
     <>
+      <Link href="/">Home</Link>
+
       <h1>Product list</h1>
 
-      <h2>Product 1</h2>
-      <h2>Product 2</h2>
-      <h2>Product 3</h2>
-      <h2>Product 4</h2>
-      <h2>Product 5</h2>
+      <h2>
+        <Link href="/products/1/reviews">Product 1 reviews</Link>
+      </h2>
+      <h2>
+        <Link href="/products/2/reviews">Product 2 reviews</Link>
+      </h2>
+      <h2>
+        <Link href="/products/3/reviews" replace>
+          Product 3 reviews
+        </Link>
+      </h2>
+
+      <h2>
+        <Link href={`/products/${productId}`}>Product {productId} details</Link>
+      </h2>
     </>
   );
 }
