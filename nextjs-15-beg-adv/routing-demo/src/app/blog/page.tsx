60f900dacd4460880e3b723db9818ba6dd305144
Muhammad Sabbir Hossain Bin Ahmad <dustobalok000@gmail.com>
2026-06-15 12:12:15 +0600

    complete: Next.js 15 Tutorial - 18 - title Metadata.

--- nextjs-15-beg-adv/routing-demo/src/app/blog/page.tsx
+++ nextjs-15-beg-adv/routing-demo/src/app/blog/page.tsx
@@ -1,3 +1,13 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: {
+    // default: "Blog", // add to parent title metadata if present
+    // template: "%s | Blog",
+    absolute: "Blog", // replace parent title metadata
+  },
+};
+
 export default function Blog() {
   return <h1>My Blog</h1>;
 }
