138326de12b8d5058fe8793b18817355f23aa2d7
Muhammad Sabbir Hossain Bin Ahmad <dustobalok000@gmail.com>
2026-06-20 16:52:23 +0600

    complete: Next.js 15 Tutorial - 57 - dynamicParams.

--- nextjs-15-beg-adv/rendering-demo/src/app/products/[id]/page.tsx
+++ nextjs-15-beg-adv/rendering-demo/src/app/products/[id]/page.tsx
@@ -1,3 +1,7 @@
+export const dynamicParams = true;
+// ture will statically render pages on demand for any dynamic segments not listed in generateStaticParams()
+// false will only statically render pages for the segments listed in generateStaticParams() and return a 404 for not included in our pre-rendered list.
+
 // generateStaticParams function runs at build time.
 export async function generateStaticParams() {
   return [{ id: "1" }, { id: "2" }, { id: "3" }];
