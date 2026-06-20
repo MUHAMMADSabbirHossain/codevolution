703ba0bd77ea2bb43f503f23b3173d44444da3ba
Muhammad Sabbir Hossain Bin Ahmad <dustobalok000@gmail.com>
2026-06-20 15:22:40 +0600

    complete: Next.js 15 Tutorial - 55 - Dynamic Rendering.

--- nextjs-15-beg-adv/rendering-demo/src/app/about/page.tsx
+++ nextjs-15-beg-adv/rendering-demo/src/app/about/page.tsx
@@ -1,8 +1,12 @@
 // import { useState } from "react";
+import { cookies } from "next/headers";
 
-export default function AboutPage() {
+export default async function AboutPage() {
   //   const [first, setFirst] = useState(0); // You're importing a module that depends on `useState` into a React Server Component module. This API is only available in Client Components. To fix, mark the file (or its parent) with the `"use client"` directive.
   console.log("About server component.");
+
+  const cookieStore = await cookies();
+  console.log(cookieStore.get("theme"));
 
   return (
     <>
