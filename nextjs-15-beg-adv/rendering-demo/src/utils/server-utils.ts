b3fc60df7cbf83e84e448fa35890720e864d1210
Muhammad Sabbir Hossain Bin Ahmad <dustobalok000@gmail.com>
2026-06-21 19:26:33 +0600

    complete: Next.js 15 Tutorial - 60 - Server-only Code.

---
+++ nextjs-15-beg-adv/rendering-demo/src/utils/server-utils.ts
@@ -0,0 +1,11 @@
+import "server only"; // server only code cannot be imported into client components
+
+export const serverSideFunction = () => {
+  console.log(
+    `use multiple libraries,
+        use environment variables, intreact with a database,
+        process confidential information`,
+  );
+
+  return "server result";
+};
