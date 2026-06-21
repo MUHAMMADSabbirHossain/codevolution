fa4cf3946dcf52040a985b8e73c151d5f8d92f20
Muhammad Sabbir Hossain Bin Ahmad <dustobalok000@gmail.com>
2026-06-21 20:35:15 +0600

    complete: Next.js 15 Tutorial - 63 - Client-only Code.

--- nextjs-15-beg-adv/rendering-demo/src/app/client-route/page.tsx
+++ nextjs-15-beg-adv/rendering-demo/src/app/client-route/page.tsx
@@ -6,10 +6,12 @@ import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import { useTheme } from "@/components/theme-provider";
+import { clientSideFunction } from "@/utils/client-utils";
 
 export default function ClientRoutePage() {
   // const result = serverSideFunction();
   const theme = useTheme();
+  const result = clientSideFunction();
 
   const settings = {
     dots: true,
@@ -72,6 +74,8 @@ export default function ClientRoutePage() {
     // </>
     <>
       <h1 style={{ color: theme.colors.primary }}>Client router page</h1>
+
+      <p>{result}</p>
     </>
   );
 }
--- nextjs-15-beg-adv/rendering-demo/src/app/server-route/page.tsx
+++ nextjs-15-beg-adv/rendering-demo/src/app/server-route/page.tsx
@@ -1,8 +1,11 @@
 import ImageSlider from "@/components/image-slider";
+// import { clientSideFunction } from "@/utils/client-utils";
 import { serverSideFunction } from "@/utils/server-utils";
+// import "client-only";
 
 export default function ServerRoutePage() {
   const result = serverSideFunction();
+  // const clientResult = clientSideFunction(); // You're importing a component that imports client-only. It only works in a Client Component but none of its parents are marked with "use client", so they're Server Components by default.
 
   const settings = {
     dots: true,
@@ -11,6 +14,7 @@ export default function ServerRoutePage() {
   return (
     <>
       <h1>Server route {result}</h1>
+      {/* <h1>{clientResult}</h1> */}
       <ImageSlider />
     </>
   );
