cd1be595b7900dd3ca1d2dbc260f0afe8447c3be
Muhammad Sabbir Hossain Bin Ahmad <dustobalok000@gmail.com>
2026-06-21 20:21:54 +0600

    complete: Next.js 15 Tutorial - 62 - Context Providers.

--- nextjs-15-beg-adv/rendering-demo/src/app/client-route/page.tsx
+++ nextjs-15-beg-adv/rendering-demo/src/app/client-route/page.tsx
@@ -5,68 +5,73 @@ import Image from "next/image";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
+import { useTheme } from "@/components/theme-provider";
 
 export default function ClientRoutePage() {
   // const result = serverSideFunction();
+  const theme = useTheme();
 
   const settings = {
     dots: true,
   };
 
   return (
+    // <>
+    //   <div className="image-slider-container">
+    //     <Slider {...settings}>
+    //       <div>
+    //         <Image
+    //           src={"https://picsum.photos/400/200"}
+    //           alt="image"
+    //           width={400}
+    //           height={200}
+    //         />
+    //       </div>
+    //       <div>
+    //         <Image
+    //           src={"https://picsum.photos/400/200"}
+    //           alt="image"
+    //           width={400}
+    //           height={200}
+    //         />
+    //       </div>
+    //       <div>
+    //         <Image
+    //           src={"https://picsum.photos/400/200"}
+    //           alt="image"
+    //           width={400}
+    //           height={200}
+    //         />
+    //       </div>
+    //       <div>
+    //         <Image
+    //           src={"https://picsum.photos/400/200"}
+    //           alt="image"
+    //           width={400}
+    //           height={200}
+    //         />
+    //       </div>
+    //       <div>
+    //         <Image
+    //           src={"https://picsum.photos/400/200"}
+    //           alt="image"
+    //           width={400}
+    //           height={200}
+    //         />
+    //       </div>
+    //       <div>
+    //         <Image
+    //           src={"https://picsum.photos/400/200"}
+    //           alt="image"
+    //           width={400}
+    //           height={200}
+    //         />
+    //       </div>
+    //     </Slider>
+    //   </div>
+    // </>
     <>
-      <div className="image-slider-container">
-        <Slider {...settings}>
-          <div>
-            <Image
-              src={"https://picsum.photos/400/200"}
-              alt="image"
-              width={400}
-              height={200}
-            />
-          </div>
-          <div>
-            <Image
-              src={"https://picsum.photos/400/200"}
-              alt="image"
-              width={400}
-              height={200}
-            />
-          </div>
-          <div>
-            <Image
-              src={"https://picsum.photos/400/200"}
-              alt="image"
-              width={400}
-              height={200}
-            />
-          </div>
-          <div>
-            <Image
-              src={"https://picsum.photos/400/200"}
-              alt="image"
-              width={400}
-              height={200}
-            />
-          </div>
-          <div>
-            <Image
-              src={"https://picsum.photos/400/200"}
-              alt="image"
-              width={400}
-              height={200}
-            />
-          </div>
-          <div>
-            <Image
-              src={"https://picsum.photos/400/200"}
-              alt="image"
-              width={400}
-              height={200}
-            />
-          </div>
-        </Slider>
-      </div>
+      <h1 style={{ color: theme.colors.primary }}>Client router page</h1>
     </>
   );
 }
