d515ccff99e08f26448d5997d20d19f6e248e983
Muhammad Sabbir Hossain Bin Ahmad <dustobalok000@gmail.com>
2026-06-22 17:58:48 +0600

    complete: Data fetching patterns is two types in inside.

---
+++ nextjs-15-beg-adv/data-fetching-demo/src/app/user-parallel/[id]/loading.tsx
@@ -0,0 +1,7 @@
+export default function LoadingPage() {
+  return (
+    <div className="flex items-center justify-center h-screen">
+      <div className="animate-bounce rounded-full h-32 w-32 border-t-8 border-b-8 border-b-red-500 border-t-blue-500"></div>
+    </div>
+  );
+}
