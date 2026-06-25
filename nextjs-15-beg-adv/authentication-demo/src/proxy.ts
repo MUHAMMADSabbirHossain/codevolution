060f69af41ad8473030dd349a6232369dc9b801c
Muhammad Sabbir Hossain Bin Ahmad <dustobalok000@gmail.com>
2026-06-25 17:05:11 +0600

    complete: Next.js 15 Tutorial - 88 - Protecting Routes.

--- nextjs-15-beg-adv/authentication-demo/src/proxy.ts
+++ nextjs-15-beg-adv/authentication-demo/src/proxy.ts
@@ -1,6 +1,31 @@
-import { clerkMiddleware } from "@clerk/nextjs/server";
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
-export default clerkMiddleware();
+// const isProtectedRoute = createRouteMatcher([
+//   "/user-profile",
+//   // "/user-profile/:path*"
+// ]);
+
+// export default clerkMiddleware(async (auth, req) => {
+//   if (isProtectedRoute(req)) {
+//     await auth.protect();
+//   }
+// });
+
+const isPublicRoute = createRouteMatcher(["/sign-in(.*)", "/sign-up(.*)"]);
+
+export default clerkMiddleware(async (auth, req) => {
+  // if (!isPublicRoute(req)) {
+  //   await auth.protect();
+  // }
+
+  const { userId, redirectToSignIn } = await auth();
+
+  if (!userId && !isPublicRoute(req)) {
+    // Add custom logic to run before redirecting
+
+    return redirectToSignIn();
+  }
+});
 
 export const config = {
   matcher: [
