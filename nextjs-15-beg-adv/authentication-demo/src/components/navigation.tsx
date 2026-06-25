23d0cb533cf37e1c808886b8f67d8a2215237880
Muhammad Sabbir Hossain Bin Ahmad <dustobalok000@gmail.com>
2026-06-25 16:44:32 +0600

    complete: Next.js 15 Tutorial - 87 - Conditional UI Rendering.

--- nextjs-15-beg-adv/authentication-demo/src/components/navigation.tsx
+++ nextjs-15-beg-adv/authentication-demo/src/components/navigation.tsx
@@ -1,4 +1,5 @@
 import {
+  Show,
   SignInButton,
   SignOutButton,
   SignUpButton,
@@ -6,7 +7,7 @@ import {
 } from "@clerk/nextjs";
 import Link from "next/link";
 
-export default function Navigation() {
+export default async function Navigation() {
   return (
     <nav className="bg-[var(--background)] border-b border-[var(--foreground)]/10">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
@@ -17,12 +18,19 @@ export default function Navigation() {
             </h1>
           </div>
           <div className="flex items-center gap-4">
-            <SignInButton mode="modal" />
-            {/* <UserButton /> */}
-
-            <Link href="/user-profile">Profile</Link>
-
-            <SignOutButton />
+            <Show
+              fallback={
+                <>
+                  <SignInButton mode="modal" />
+                  <SignUpButton mode="modal" />
+                </>
+              }
+              when="signed-in"
+            >
+              <UserButton />
+              <Link href="/user-profile">Profile</Link>
+              <SignOutButton />
+            </Show>
           </div>
         </div>
       </div>
