9f7832ec5f2e680f66ad7dd63686da3c9df156a2
Muhammad Sabbir Hossain Bin Ahmad <dustobalok000@gmail.com>
2026-06-25 16:16:18 +0600

    complete: Next.js 15 Tutorial - 86 - Profile Settings.

--- nextjs-15-beg-adv/authentication-demo/src/components/navigation.tsx
+++ nextjs-15-beg-adv/authentication-demo/src/components/navigation.tsx
@@ -1,4 +1,9 @@
-import { SignInButton, SignOutButton, SignUpButton } from "@clerk/nextjs";
+import {
+  SignInButton,
+  SignOutButton,
+  SignUpButton,
+  UserButton,
+} from "@clerk/nextjs";
 import Link from "next/link";
 
 export default function Navigation() {
@@ -12,7 +17,11 @@ export default function Navigation() {
             </h1>
           </div>
           <div className="flex items-center gap-4">
-            <SignInButton mode="modal"></SignInButton>
+            <SignInButton mode="modal" />
+            {/* <UserButton /> */}
+
+            <Link href="/user-profile">Profile</Link>
+
             <SignOutButton />
           </div>
         </div>
